8c252c25685520fb99b0559d4ab694f8b0c99858
ahnafms <ahnafmusyaffa47@gmail.com>
2023-03-10 23:51:04 +0700

    feat: add file output feature

--- src/App.vue
+++ src/App.vue
@@ -3,48 +3,106 @@
     <div class="w-fit mx-auto">
       <div class="text-7xl">hgallo</div>
       <textarea class="" v-model="text" type="text" name="" placeholder="send text" id="" />
-      <select v-model="voiceSelect" name="" id="voiceSelect"></select>
-      <button @click="speechToText" type="submit">Submit</button>
-      <button @click="voiceList">tes</button>
+      <select v-model="voiceSelect" name="" id="voiceSelect" ref="voiceSelect">
+        <option data-name="Daniel" data-lang="en-GB" selected>Daniel - (en-GB) - DEFAULT</option>
+        <option
+          v-for="voice in voiceList"
+          :data-name="voice.name"
+          :data-lang="voice.lang"
+          :key="voice"
+        >
+          {{ voice.name }} - {{ voice.lang }}
+        </option>
+      </select>
+      <input id="pitch" type="range" v-model="pitch" min="0" max="2" step="0.2" />
+      <input id="rate" type="range" v-model="rate" min="0" max="10" step="0.5" />
+      <button @click="statePause">{{ this.isPause ? 'Resume' : 'Pause' }}</button>
+      <button @click="stateCancel">Cancel</button>
+      <button @click="speechToText" type="submit">Play</button>
+      <a id="downloadLink" download="" @click="" href=""></a>
     </div>
   </div>
 </template>
-
 <script>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-
+import Recorder from 'recorder-js'
 export default {
   data() {
     return {
+      utterance: '',
+      isPause: false,
       text: '',
+      pitch: 1,
+      rate: 0,
       voiceSelect: '',
-      voiceList: []
-    }
-  },
-  mounted() {
-    const synth = window.speechSynthesis
-    this.voiceList = synth.getVoices()
-    for (let i = 0; i < this.voiceList.length; i++) {
-      const option = document.createElement('option')
-      option.textContent = `${this.voiceList[i].name} - (${this.voiceList[i].lang})`
-      if (this.voiceList[i].default) {
-        option.textContent += ' - DEFAULT'
-      }
-      option.setAttribute('data-lang', this.voiceList[i].lang)
-      option.setAttribute('data-name', this.voiceList[i].name)
-      document.getElementById('voiceSelect').appendChild(option)
+      voiceList: [
+        {
+          name: 'Samantha',
+          lang: 'en-US'
+        },
+        {
+          name: 'Damayanti',
+          lang: 'id-ID'
+        }
+      ]
     }
   },
   methods: {
     speechToText() {
-      let speech = new SpeechSynthesisUtterance(this.text)
-      for (let i = 0; i < this.voiceList.length; i++) {
-        if (this.voiceList[i].name === this.voiceSelect.split(' ')[0]) {
-          speech.voice = this.voiceList[i]
+      this.utterance = new SpeechSynthesisUtterance(this.text)
+      const populateVoiceList = window.speechSynthesis.getVoices()
+      for (let i = 0; i < populateVoiceList.length; i++) {
+        if (
+          populateVoiceList[i].name === this.voiceSelect.split(' - ')[0] &&
+          populateVoiceList[i].lang === this.voiceSelect.split(' - ')[1]
+        ) {
+          this.utterance.voice = populateVoiceList[i]
+          this.utterance.lang = populateVoiceList[i].lang
+          this.utterance.pitch = this.pitch
+          this.utterance.rate = this.rate
+          this.utterance.volume = 100.0
         }
       }
-      speechSynthesis.speak(speech)
+      this.generateAudioDownload()
+    },
+    statePause() {
+      if (this.isPause == true) {
+        this.isPause = false
+        speechSynthesis.resume(this.utterance)
+      } else {
+        speechSynthesis.pause(this.utterance)
+        this.isPause = true
+      }
+    },
+    stateCancel() {
+      speechSynthesis.cancel(this.utterance)
+    },
+
+    generateAudioDownload() {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+          const mediaRecorder = new MediaRecorder(stream)
+          const audioData = []
+          mediaRecorder.addEventListener('dataavailable', (event) => {
+            audioData.push(event.data)
+          })
+          mediaRecorder.addEventListener('stop', () => {
+            const blob = new Blob(audioData, { type: 'audio/wav' })
+            const url = URL.createObjectURL(blob)
+            const link = document.getElementById('downloadLink')
+            link.href = url
+            link.download = 'synthesized-audio.wav'
+            document.getElementById('downloadLink').textContent = link.download
+          })
+          mediaRecorder.start()
+          speechSynthesis.speak(this.utterance)
+          this.utterance.addEventListener('end', () => {
+            mediaRecorder.stop()
+          })
+        })
+        .catch((error) => {
+          console.error('Failed to get user media', error)
+        })
     }
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define:{
+    "global":{}
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
